62ebd9a97301c916e4aadccccfdfe84e4439bd55
Prasanth <prasanthcena968@gmail.com>
2022-12-13 18:04:17 +0530

    Initial commit

---
+++ app/src/main/java/com/skyblue/camera2/MainActivity.java
@@ -0,0 +1,20 @@
+package com.skyblue.camera2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (null == savedInstanceState){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, Camera2BasicFragment.newInstance())
+                    .commit();
+        }
+    }
+}
